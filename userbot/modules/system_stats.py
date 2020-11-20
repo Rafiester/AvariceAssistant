a9547d4839580afcef5a5dfd639e7ace68e36a49
Rafi Ramadhan <rafi.haqqi@gmail.com>
2020-11-21 02:05:57 +0700

    system_stats.py: Change to simple alive

--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -206,12 +206,12 @@ async def amireallyalive(alive):
     img = IMG
     caption = (
         ""
-        "こんにちは!!! ~ HANA-CI Userbot Here (˃ᴗ˂) ~\n\n"
-        f"👤 User             : ` Nicklas373`\n"
-        f"🐍 Python           : ` {python_version()}`\n"
-        f"💻 Telethon version : ` {version.__version__}`\n"
-        f"🕒 Bot Uptime       : ` {uptime}`\n"
-        ""
+                     f"------------------------------------\n"
+                     f"•  Master           : {DEFAULTUSER}\n"
+                     f"•  Python           : {python_version()}\n"
+                     f"•  Telethon version : {version.__version__}\n"
+                     f"------------------------------------\n"
+                     ""
     )
     await bot.send_file(alive.chat_id, img, caption=caption)
     await alive.delete()
